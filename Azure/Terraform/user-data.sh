b9cb7d94ed762e7d9dc9f4a67010cc6134a51193
Jon Spriggs <jon@sprig.gs>
2019-09-23 14:49:46 +0100

    Fixed typo in user-data.

--- Azure/Terraform/user-data.sh
+++ Azure/Terraform/user-data.sh
@@ -1,4 +1,4 @@
 #! /bin/bash -x
 apt install -y apache2
 echo "<html><head><title>$(hostname)</title></head><body><h1>$(hostname)</h1></body></html>" > /var/www/html/index.html
-/bin/systemctl start httpd
+/bin/systemctl start apache2
